2a144306d2047627dd005a5c508c7a1e5894fd4e
shsushmitha <40930627+shsushmitha@users.noreply.github.com>
2018-08-06 10:29:10 +0530

    My First Commit

---
+++ Day1-GitDemo/src/main/java/org/cap/demo/MainClass.java
@@ -0,0 +1,15 @@
+package org.cap.demo;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+		greet();
+	}
+	
+	public static void greet() {
+		System.out.println("Hello");
+	}
+
+}
