6540a69f80fcefa7c4274c03a364ab22ef5f2300
shsushmitha <40930627+shsushmitha@users.noreply.github.com>
2018-08-06 10:52:49 +0530

    My second commit

--- Day1-GitDemo/src/main/java/org/cap/demo/MainClass.java
+++ Day1-GitDemo/src/main/java/org/cap/demo/MainClass.java
@@ -10,6 +10,11 @@ public class MainClass {
 	
 	public static void greet() {
 		System.out.println("Hello");
+		System.out.println("How are you?");
+		System.out.println("Be happy");
+		for(int i=0;i<5;i++) {
+			System.out.println(i);
+		}
 	}
 
 }
