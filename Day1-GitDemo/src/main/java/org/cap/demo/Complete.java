eb30280dbcdef90d57835f61f151e4695101dd22
shsushmitha <40930627+shsushmitha@users.noreply.github.com>
2018-08-06 12:17:13 +0530

    Complete commit

---
+++ Day1-GitDemo/src/main/java/org/cap/demo/Complete.java
@@ -0,0 +1,9 @@
+package org.cap.demo;
+
+public class Complete {
+
+public void Display() {
+	System.out.println("hi all, My work is completed");
+}
+
+}
--- Day1-GitDemo/src/main/java/org/cap/demo/MainClass.java
+++ Day1-GitDemo/src/main/java/org/cap/demo/MainClass.java
@@ -5,9 +5,11 @@ public class MainClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		SalesClass class1=new SalesClass();
+		Complete class2=new Complete();
 		System.out.println("Hello World!");
 		greet();
 		class1.show();
+		class2.Display();
 	}
 	
 	public static void greet() {
